7f51b4d701d252476de6b4b6fc028896ba06aa4d
My Nguyen <myntuit@gmail.com>
2023-01-12 20:39:57 +0900

    Remove Plant related stuff

--- database/src/main/java/com/mynt/demo/database/DemoDatabase.kt
+++ database/src/main/java/com/mynt/demo/database/DemoDatabase.kt
@@ -2,15 +2,11 @@ package com.mynt.demo.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.mynt.demo.database.dao.PlantDao
-import com.mynt.demo.database.model.PlantEntity
 
-@Database(
-    entities = [
-        PlantEntity::class
-    ],
-    version = 1
-)
+//@Database(
+//    entities = [
+//    ],
+//    version = 1
+//)
 abstract class DemoDatabase : RoomDatabase() {
-    abstract fun plantDao(): PlantDao
 }
--- database/src/main/java/com/mynt/demo/database/data/LocalDataSourceModule.kt
+++ database/src/main/java/com/mynt/demo/database/data/LocalDataSourceModule.kt
@@ -1,6 +1,5 @@
 package com.mynt.demo.database.data
 
-import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -8,10 +7,5 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LocalDataSourceModule {
-
-    @Binds
-    abstract fun bindsPlantLocalDataSource(
-        plantLocalDataSourceImpl: PlantLocalDataSourceImpl
-    ): PlantLocalDataSource
 
 }
